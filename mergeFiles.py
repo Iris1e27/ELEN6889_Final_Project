59115bc7f698fd77bb10d0862e6825069a7b67fd
zjt <jingtianzhang27@gmail.com>
2023-04-28 12:22:00 -0400

    dataset merged all

--- mergeFiles.py
+++ mergeFiles.py
@@ -2,9 +2,9 @@ import os
 import pandas as pd
 
 # 设置csv文件所在的文件夹路径
-# folder_path = 'E:\\ColumbiaUniversity\\ELEN6889\\Project'
-#folder_path = 'E:\\ColumbiaUniversity\\ELEN6889\\Project\\dataset\\name'
-folder_path = 'E:\\ColumbiaUniversity\\ELEN6889\\Project\\dataset\\place+name'
+folder_path = 'E:\\ColumbiaUniversity\\ELEN6889\\Project'
+# folder_path = 'E:\\ColumbiaUniversity\\ELEN6889\\Project\\dataset\\name'
+# folder_path = 'E:\\ColumbiaUniversity\\ELEN6889\\Project\\dataset\\place+name'
 
 # 读取文件夹中的所有csv文件
 files = [file for file in os.listdir(folder_path) if file.endswith('.csv')]
@@ -19,5 +19,5 @@ for file in files:
     df = pd.concat([df, temp_df])
 
 # 保存合并后的DataFrame到一个新的csv文件中
-merged_file_path = "mergedPlaceName.csv"
+merged_file_path = "mergedAll.csv"
 df.to_csv(merged_file_path, index=False)
